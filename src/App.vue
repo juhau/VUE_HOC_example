e48beb30828a94399c758470406aadc2370cdce9
juhau <juhau@users.noreply.github.com>
2018-11-30 11:24:32 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png" />
+    <hello-world />
+    <hr />
+    <div>test avec JS inheritance</div>
+    <Hello />
+    <hr />
+    <div>test avec scope</div>
+    <Hello> <hello-world slot-scope="hello" :message="hello.message" /> </Hello>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld";
+import Hello from "./components/Hello";
+
+export default {
+  name: "App",
+  components: {
+    helloWorld: HelloWorld,
+    Hello: Vue.extend(HelloWorld).extend(Hello)
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>Hello {{ msg }}</h1>
+    <h2>{{ test }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  data() {
+    return {
+      msg: "World",
+      test: "message from Hello Component"
+    };
+  }
+};
+</script>
